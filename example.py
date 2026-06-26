d5aa0d85cabe77013f8c45fb401415a2063b703f
NirmalUS <142925483+NirmalUS@users.noreply.github.com>
2026-06-26 12:33:36 +0530

    Added complete example file

--- example.py
+++ example.py
@@ -4,5 +4,5 @@ from GAIA_HR import query
 from GAIA_HR import plot
 
 df = query.fetch_gaia_data(ra = 136, dec = 16, radius = 1, d_min=30, condition = "teff_gspphot > 2000")
-# plot.plot_hr(df)
-# plot.plot_radec(df)
+plot.plot_hr(df, property_name="radius_flame", cmap="plasma", log_plot=True)
+# plot.plot_radec(df, property_name="radius_flame", cmap="plasma", log_plot=True)
