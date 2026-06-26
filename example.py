c1ca0bef40310f35fb7a42440b5f74afd6ba303f
NirmalUS <142925483+NirmalUS@users.noreply.github.com>
2026-06-26 17:08:52 +0530

    Improved example.py

--- example.py
+++ example.py
@@ -6,3 +6,6 @@ from GAIA_HR import plot
 df = query.fetch_gaia_data(ra = 136, dec = 16, radius = 1, d_min=30, server="aip")
 plot.plot_hr(df, property_name="radius_flame", cmap="plasma", log_plot=True)
 # plot.plot_radec(df, property_name="radius_flame", cmap="plasma", log_plot=True)
+# Available colourmap parameters : ra, dec, parallax, g_mean_mag, bp_rp, teff, logg, mh, pm, rv, lum_flame,
+# radius_flame, mass_flame
+
