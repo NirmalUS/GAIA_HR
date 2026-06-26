02a5e7668d921345a7247ab6c6fa412de5f093fa
NirmalUS <nirmalus9bsnvm@gmail.com>
2026-06-26 05:29:15 +0000

    Added test folder and file

---
+++ test/test_GAIA_HR.py
@@ -0,0 +1,23 @@
+import pytest
+import pandas as pd
+from GAIA_HR import query
+from GAIA_HR import plot
+
+
+def test_fetch_gaia_data():
+    """
+    Unit test to evaluate correctness of fetch_gaia_data() function
+    """
+    pass
+
+def test_plot_hr():
+    """
+    Unit test to evaluate correctness of plot_test() function
+    """
+    pass
+
+def end_to_end_test():
+    """
+    End-to-end test to evaluate correctness of GAIA_HR module
+    """
+    pass
